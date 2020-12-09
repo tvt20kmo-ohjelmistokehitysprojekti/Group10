78a3273cf7496aa03ea2a3efd9b1c7c02045e2f7
Roope Karjalainen <c9karo00@students.oamk.fi>
2020-12-09 23:05:23 +0200

    Restapi fixausta

--- RestApi/application/config/database.php
+++ RestApi/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'ci_user',
-	'password' => 'ci_pass',
-	'database' => 'ci_rest_api',
+	'hostname' => 'mysli.oamk.fi',
+	'username' => 't9ylja01',
+	'password' => 'perse',
+	'database' => 'opisk_t9ylja01',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
---
+++ RestApi/application/models/User_model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ */
+class User_model extends CI_model
+{
+  function get_user($id){
+    $this->db->select('Balance');
+    $this->db->from('Account');
+    if($id !== NULL) {
+      $this->db->where('idAccount',$id);
+    }
+    return $this->db->get()->result_array();
+  }
+
+}
