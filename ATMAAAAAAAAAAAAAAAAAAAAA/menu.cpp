6ad0d3214e5a81077ca7c54305bba4446e58e671
bobtheburgerflipper <t9jojo01@students.oamk.fi>
2020-12-11 01:35:25 +0200

    kaikki vituillaan saatana

--- ATMAAAAAAAAAAAAAAAAAAAAA/menu.cpp
+++ ATMAAAAAAAAAAAAAAAAAAAAA/menu.cpp
@@ -1,17 +1,17 @@
 #include "menu.h"
 #include "ui_menu.h"
-#include <QDebug>
-#include <QJsonDocument>
-#include <QJsonObject>
 #include <QNetworkAccessManager>
 #include <QtNetwork>
-#include <QNetworkAccessManager>
 
-menu::menu(QWidget *parent) :
+
+menu::menu(const QString &idcard, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::menu)
 {
     ui->setupUi(this);
+
+    id = idcard;
+
 
     manager = new QNetworkAccessManager();
     QObject::connect(manager, &QNetworkAccessManager::finished,
@@ -21,12 +21,12 @@ menu::menu(QWidget *parent) :
                 return;
             }
 
-            QString answer = reply->readAll();
-
-             ui->label_balance->setText(answer+ " €");
+            answer = reply->readAll();
 
             qDebug() << answer;
+
         }
+
     );
 
 }
@@ -38,20 +38,43 @@ menu::~menu()
 
 void menu::on_refresh_clicked()
 {
-    QString concatenated = "admin:1234";
-    QByteArray data = concatenated.toLocal8Bit().toBase64();
-    QString headerData = "Basic " + data;
-
-    request.setUrl(QUrl("http://www.students.oamk.fi/~c9karo00/Group10/RestApi/index.php/api/Balance/?id=1"));
-
-    request.setRawHeader("Authorization", headerData.toLocal8Bit());
-
+    request.setUrl(QUrl("http://www.students.oamk.fi/~c9karo00/Group10/RestApi/index.php/api/Balance/?id="+id));
+    basicAuth();
     manager->get(request);
 
+    answer.remove(QRegExp(QString::fromUtf8("\"")));
+    ui->label_balance->setText(answer+" €");
 
+    qDebug() << "id: "+id;
 }
 
 void menu::on_btn_20_clicked()
 {
-
+    amount = "20";
+    qDebug() << amount;
 }
+
+
+void menu::on_btn_withdraw_clicked()
+{
+
+    request.setUrl(QUrl("http://www.students.oamk.fi/~c9karo00/Group10/RestApi/index.php/api/withdraw"));
+    basicAuth();
+
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+
+    QUrlQuery params;
+    params.addQueryItem("idAccount", id);
+    params.addQueryItem("amount", amount);
+
+    manager->post(request, params.query().toUtf8());
+}
+
+void menu::basicAuth()
+{
+    QString concatenated = "admin:1234";
+    QByteArray data = concatenated.toLocal8Bit().toBase64();
+    QString headerData = "Basic " + data;
+    request.setRawHeader("Authorization", headerData.toLocal8Bit());
+}
+
--- ATMAAAAAAAAAAAAAAAAAAAAA/menu.h
+++ ATMAAAAAAAAAAAAAAAAAAAAA/menu.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QNetworkAccessManager>
+#include "mainwindow.h"
 
 namespace Ui {
 class menu;
@@ -13,18 +14,28 @@ class menu : public QWidget
     Q_OBJECT
 
 public:
-    explicit menu(QWidget *parent = nullptr);
+    explicit menu(const QString &idcard, QWidget *parent = nullptr);
     ~menu();
+
+
 
 private slots:
     void on_refresh_clicked();
 
     void on_btn_20_clicked();
 
+    void basicAuth();
+
+    void on_btn_withdraw_clicked();
+
 private:
     Ui::menu *ui;
     QNetworkAccessManager *manager;
     QNetworkRequest request;
+    QString id, amount, answer;
+
+
+
 };
 
 #endif // MENU_H
